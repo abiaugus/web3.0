b21f86f38a65033da83a95fde023fa8c1a25bdd8
abiaugus <appus525@gmail.com>
2022-01-18 11:39:28 +0530

    footer

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -2,14 +2,24 @@ import { useEffect, useState } from "react";
 
 const API_KEY = import.meta.env.VITE_GIPHY_API;
 
-const useFetch = () => {
+const useFetch = ({keyword}) => {
     const [gifUrl, setGifUrl] = useState("");
 
-    const fetchGifs = aysnc () => {
+    const fetchGifs = async() => {
         try {
-            const response = 
+            const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword.split(" ").join("")}&limit=1`);
+            const {data} = await response.json()
+            setGifUrl(data[0]?.images?.downsized_medium?.url)
         } catch (error) {
-            
+            console.log("fetch error");
         }
     }
+
+    useEffect(() => {
+        if(keyword) fetchGifs();
+    }, [keyword])
+
+    return gifUrl;
 }
+
+export default useFetch;
